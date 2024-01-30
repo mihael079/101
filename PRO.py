b3497bdf8c939d873defc5028e9a62e4a2d63f36
mihael079 <91776070+mihael079@users.noreply.github.com>
2024-01-30 12:07:14 +0800

    Add files via upload

---
+++ PRO.py
@@ -0,0 +1,11 @@
+import random
+
+response=input("Choose y or n: ")
+
+if(response=="y"):
+
+        yes=random.randint(1,6)
+        print(yes)
+elif(response=="n"):
+        print("quitting")
+    
